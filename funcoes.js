e94d02407c0fb2b5423c4347f1c999f810131817
Willian Miguel Santos da Silva <willianmiguel6440@gmail.com>
2022-03-16 17:30:08 -0300

    Imersão Dev Alura - Aula 01

---
+++ funcoes.js
@@ -0,0 +1,18 @@
+var nome = "Willian"
+
+var notaDoPrimeiroBimestre = 10
+var notaDoSegundoBimestre = 10
+var notaDoTerceiroBimestre = 10
+var notaDoQuartoBimestre = 10
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// isso é um comentário 
+
+// Revisão
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
